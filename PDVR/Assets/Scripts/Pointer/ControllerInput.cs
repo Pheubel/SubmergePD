07af70e17ed744272b93a2eb55ace41f837a4836
michael <mikey0812@gmail.com>
2020-02-27 13:31:33 +0100

    Laser pointer implementation

---
+++ PDVR/Assets/Scripts/Pointer/ControllerInput.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Valve.VR;
+
+public class ControllerInput : BaseInput
+{
+    [Header("Actions")]
+    public SteamVR_Action_Boolean squeeze;
+    [SerializeField] private SteamVR_Input_Sources _hands;
+
+
+    public Camera eventCamera = null;
+
+
+
+
+    protected override void Awake()
+    {
+        GetComponent<BaseInputModule>().inputOverride = this;
+    }
+
+    void Start()
+    {
+        squeeze.AddOnStateDownListener(TriggerDown, _hands);
+        squeeze.AddOnStateUpListener(TriggerUp, _hands);
+    }
+
+    public void TriggerUp(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        print("up");
+    }
+    public void TriggerDown(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        print("down");
+    }
+
+
+    public override Vector2 mousePosition
+    {
+        get
+        {
+            return new Vector2(eventCamera.pixelWidth / 2, eventCamera.pixelHeight / 2);
+        }
+    }
+}
---
+++ PDVR/Assets/Scripts/Pointer/PhysicsPointer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsPointer : MonoBehaviour
+{
+    public float defaultLength = 7.0f;
+
+    private LineRenderer lineRenderer = null;
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    void Update()
+    {
+        UpdateLength();
+    }
+
+    private void UpdateLength()
+    {
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, CalculateEnd());
+
+    }
+    private Vector3 CalculateEnd()
+    {
+        RaycastHit hit = CreateForwardRayCast();
+        Vector3 endPositon = DefaultEnd(defaultLength);
+
+        if (hit.collider)
+            endPositon = hit.point;
+
+        return endPositon;
+    }
+
+    private RaycastHit CreateForwardRayCast()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, transform.forward);
+
+        Physics.Raycast(ray, out hit, defaultLength);
+        return hit;
+    }
+
+    private Vector3 DefaultEnd(float length)
+    {
+        return transform.position + (transform.forward * length);
+    }
+}
