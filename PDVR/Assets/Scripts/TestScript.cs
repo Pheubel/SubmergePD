3a4aaf97f938ee216e24fdb649ba2e9bc0573eea
Pheubel <luca.guagliardo@live.nl>
2020-03-03 13:00:04 +0100

    created test script

---
+++ PDVR/Assets/Scripts/TestScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class TestScript : MonoBehaviour
+{
+    [SerializeReference] AudioData _data;
+    [SerializeField] AudioSource _source;
+    [SerializeField] private bool recording;
+
+    Coroutine loadProcces;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(recording && Input.GetKeyUp(KeyCode.R))
+        {
+            recording = false;
+        }
+
+        if(!recording && Input.GetKeyDown(KeyCode.R))
+        {
+            recording = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            loadProcces = StartCoroutine(_data.LoadAudio());
+        }
+
+        if (Input.GetKeyDown(KeyCode.P) && _source.clip != null)
+            _source.Play();
+    }
+}
