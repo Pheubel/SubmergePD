b7aee8c89c073d9994229004caeea509067df5c5
Pheubel <luca.guagliardo@live.nl>
2020-03-03 12:59:43 +0100

    created audio data

---
+++ PDVR/Assets/Scripts/ScriptableObjects/AudioData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+[Serializable]
+public sealed class AudioData : ScriptableObject
+{
+    [SerializeField] private string _audioLocation;
+    [SerializeField] private string _nameRecorder;
+    [SerializeField] private Vector3 _location;
+    [SerializeField] private AudioType _audioType;
+
+    public AudioClip Clip { get; private set; }
+    public string AudioLocation => _audioLocation;
+    public string NameRecorder => _nameRecorder;
+    public Vector3 Location => _location;
+    public AudioType AudioType => _audioType;
+
+    public void Instantiate(string json) => JsonUtility.FromJsonOverwrite(json, this);
+
+
+    public IEnumerator LoadAudio()
+    {
+        using (var request = UnityWebRequestMultimedia.GetAudioClip(_audioLocation, _audioType))
+        {
+            yield return request.SendWebRequest();
+
+            if (request.isNetworkError || request.isHttpError)
+            {
+                Debug.LogError("Error: " + request.error);
+            }
+
+            Clip = DownloadHandlerAudioClip.GetContent(request);
+        }
+    }
+}
