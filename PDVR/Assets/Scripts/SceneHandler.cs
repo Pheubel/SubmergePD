4439d5ed4cdb91da38729cf51bff2b6f55fa4128
Michael <mikey0812@gmail.com>
2020-03-12 16:50:34 +0100

    Menu button functionalities

--- PDVR/Assets/Scripts/SceneHandler.cs
+++ PDVR/Assets/Scripts/SceneHandler.cs
@@ -15,6 +15,10 @@ public class SceneHandler : MonoBehaviour
     public LineRenderer line;
 
     public SteamVR_LaserPointer laserPointer;
+    public GameObject prefab; // This is our prefab object that will be exposed in the inspector
+
+    public int numberToCreate; // number of objects to create. Exposed in inspector
+
 
     void Awake()
     {
@@ -22,6 +26,11 @@ public class SceneHandler : MonoBehaviour
         laserPointer.PointerOut += PointerOutside;
         laserPointer.PointerSqueeze += PointerSqueeze;
         laserPointer.PointerRelease += PointerRelease;
+    }
+
+    void Start()
+    {
+        Populate(3);
     }
 
     public void PointerSqueeze(object sender, PointerEventArgs e)
@@ -35,9 +44,25 @@ public class SceneHandler : MonoBehaviour
         {
             Debug.Log("Cube was clicked");
         }
-        else if (e.target.name == "Button")
+        else if (e.target.name == "ButtonFotos")
         {
-            Debug.Log("Button was clicked");
+            Debug.Log("Fotos Button was clicked");
+            Populate(3);
+        }
+        else if (e.target.name == "ButtonBewijstukken")
+        {
+            Debug.Log("Bewijs Button was clicked");
+            Populate(8);
+        }
+        else if (e.target.name == "ButtonModels")
+        {
+            Debug.Log("Models  Button was clicked");
+            Populate(2);
+        }
+        else if (e.target.name == "ButtonFavorieten")
+        {
+            Debug.Log("Favorieten Button was clicked");
+            Populate(15);
         }
 
     }
@@ -45,11 +70,12 @@ public class SceneHandler : MonoBehaviour
     public void PointerInside(object sender, PointerEventArgs e)
     {
 
+        Debug.Log(e.target.name);
         if (e.target.name == "Cube")
         {
             Debug.Log("Cube was entered");
         }
-        else if (e.target.name == "Button")
+        else if (e.target.name =="Button")
         {
             Image buttonImage = e.target.GetComponent<Image>();
             buttonImage.color = Color.red;
@@ -80,5 +106,36 @@ public class SceneHandler : MonoBehaviour
         print(e.target.position);
         line.SetPosition(1, e.target.position);
 
+    }
+
+    void Populate(int numberOfItems)
+    {
+        ClearChildren();
+        GameObject newObj; // Create GameObject instance
+
+        for (int i = 0; i < numberOfItems; i++)
+        {
+            // Create new instances of our prefab until we've created as many as we specified
+            newObj = (GameObject)Instantiate(prefab, transform);
+
+        }
+
+    }
+
+    void ClearChildren()
+    {
+        int i = 0;
+        GameObject[] allChildren = new GameObject[prefab.transform.childCount];
+        foreach (GameObject child in prefab.transform)
+        {
+            allChildren[i] = child.gameObject;
+            i += 1;
+        }
+
+        foreach (GameObject child in allChildren)
+        {
+            DestroyImmediate(child.gameObject);
+        }
+      
     }
 }
