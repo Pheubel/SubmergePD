43daed6b94deeb540bbe3bd68a4458cf41fddadc
Michael <mikey0812@gmail.com>
2020-03-12 14:17:10 +0100

    Box colliders added to buttons

--- PDVR/Assets/Scripts/SceneHandler.cs
+++ PDVR/Assets/Scripts/SceneHandler.cs
@@ -31,11 +31,28 @@ public class SceneHandler : MonoBehaviour
 
         print(e.target.position);
         line.SetPosition(0, e.target.position);
+        if (e.target.name == "Cube")
+        {
+            Debug.Log("Cube was clicked");
+        }
+        else if (e.target.name == "Button")
+        {
+            Debug.Log("Button was clicked");
+        }
 
     }
 
     public void PointerInside(object sender, PointerEventArgs e)
     {
+
+        if (e.target.name == "Cube")
+        {
+            Debug.Log("Cube was entered");
+        }
+        else if (e.target.name == "Button")
+        {
+            Debug.Log("Button was entered");
+        }
         if (e.target == null)
             return;
 
@@ -45,6 +62,15 @@ public class SceneHandler : MonoBehaviour
 
     public void PointerOutside(object sender, PointerEventArgs e)
     {
+        if (e.target.name == "Cube")
+        {
+            Debug.Log("Cube was exited");
+        }
+        else if (e.target.name == "Button")
+        {
+            Debug.Log("Button was exited");
+        }
+
         MeshRenderer meshRenderer = e.target.GetComponent<MeshRenderer>();
         meshRenderer.material.color = normalColor;
     }
