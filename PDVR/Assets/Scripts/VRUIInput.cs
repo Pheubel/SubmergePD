8c85773971d255fdd1c845ded4fac7477897c3b5
Pheubel <luca.guagliardo@live.nl>
2020-05-12 19:09:54 +0200

    added basic laserpointer functionality

---
+++ PDVR/Assets/Scripts/VRUIInput.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.Extras;
+
+[RequireComponent(typeof(SteamVR_LaserPointer))]
+public class VRUIInput : MonoBehaviour
+{
+    private SteamVR_LaserPointer _laserPointer;
+
+    private void Start()
+    {
+        _laserPointer = GetComponent<SteamVR_LaserPointer>();
+    }
+
+    private void OnEnable()
+    {
+        _laserPointer.PointerIn += HandlePointerIn;
+        _laserPointer.PointerOut += HandlePointerOut;
+        _laserPointer.PointerClick += HandlePointerClick;
+    }
+
+    private void OnDisable()
+    {
+        _laserPointer.PointerIn -= HandlePointerIn;
+        _laserPointer.PointerOut -= HandlePointerOut;
+        _laserPointer.PointerClick -= HandlePointerClick;
+    }
+
+    private void HandlePointerIn(object sender, PointerEventArgs e)
+    {
+        // entered object
+    }
+
+    private void HandlePointerOut(object sender, PointerEventArgs e)
+    {
+        // exited object
+    }
+
+    private void HandlePointerClick(object sender, PointerEventArgs e)
+    {
+        // clicked object
+        if (e.target.TryGetComponent<LaserPointerInteractable>(out var interactable))
+            interactable.TriggerInteractable();
+    }
+}
