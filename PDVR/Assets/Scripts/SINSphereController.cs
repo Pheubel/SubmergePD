205bd092aca2e8b0fb9bc11da9509b56061bd9d7
Pheubel <luca.guagliardo@live.nl>
2020-05-25 10:42:16 +0200

    added sinspere controller

---
+++ PDVR/Assets/Scripts/SINSphereController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class SINSphereController : MonoBehaviour
+{
+    [SerializeField] GameObject _openSphere;
+    [SerializeField] AudioBubbleController _audioBubble;
+
+    public void OpenSphere()
+    {
+        gameObject.SetActive(false);
+        _openSphere.SetActive(true);
+
+        var audioData = ScriptableObject.CreateInstance<AudioData>();
+        audioData.Instantiate($"file://{Path.Combine(Application.streamingAssetsPath, "clips", "test 1.wav")}", _audioBubble.transform.position,AudioType.WAV);
+        StartCoroutine(audioData.LoadAudio());
+        _audioBubble.Initialize(audioData);
+    }
+}
