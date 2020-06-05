731eadcb13e6cc363259a3fb55615f96bcd44e46
Pheubel <luca.guagliardo@live.nl>
2020-06-05 12:55:15 +0200

    added teleport event for onboarding

--- PDVR/Assets/SteamVRImport/SteamVR/InteractionSystem/Teleport/Scripts/Teleport.cs
+++ PDVR/Assets/SteamVRImport/SteamVR/InteractionSystem/Teleport/Scripts/Teleport.cs
@@ -15,6 +15,9 @@ namespace Valve.VR.InteractionSystem
     {
         public SteamVR_Action_Boolean teleportAction = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("Teleport");
 
+		public UnityEvent OnTeleport => _onTeleport;
+	    [SerializeField] private UnityEvent _onTeleport;
+
         public LayerMask traceLayerMask;
 		public LayerMask floorFixupTraceLayerMask;
 		public float floorFixupMaximumTraceDistance = 1.0f;
@@ -903,6 +906,7 @@ namespace Valve.VR.InteractionSystem
 			}
 
 			Teleport.Player.Send( pointedAtTeleportMarker );
+			_onTeleport?.Invoke();
 		}
 
 
