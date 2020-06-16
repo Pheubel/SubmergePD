3c16f7b5cd7fd00d4bf798716218b64ead0fc1a7
Pheubel <luca.guagliardo@live.nl>
2020-06-16 10:51:58 +0200

    added fade animation

---
+++ PDVR/Assets/Scripts/TransitionConroller.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionConroller : MonoBehaviour
+{
+    public string NextScene => _nextScene;
+    [SerializeField] private string _nextScene;
+
+    private Animator _animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+
+        _animator.SetTrigger("FadeInCalled");
+    }
+
+    public void OnFadeInCompleted()
+    {
+
+    }
+
+    public void OnFadeOutCompleted()
+    {
+        SceneManager.LoadScene(_nextScene);
+    }
+
+    public void PlayFadeOut()
+    {
+        _animator.SetTrigger("FadeOutCalled");
+    }
+}
